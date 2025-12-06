0925668aa717c1735651d239e35c08916f88bb6d
Marian Toma <164668242+MarianToma5@users.noreply.github.com>
2025-12-06 16:21:54 +0200

    Add organizers and utility routers to app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const eventsRouter = require("./routes/events.routes");
+const organizersRouter = require("./routes/organizers.routes"); 
+const utilityRouter = require("./routes/utility.routes"); 
 
 const app = express();
 
@@ -8,8 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/events", eventsRouter);
+app.use("/api/organizers", organizersRouter);
+app.use("/api/utils", utilityRouter);
 
-// handler simplu pentru 404
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
